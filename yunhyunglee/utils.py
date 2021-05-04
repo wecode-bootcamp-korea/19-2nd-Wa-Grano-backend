33304dbbc8719f62e12122e0e36e637e0c858a04
Yunhyung Lee <yunhyunglee@Yunhyung-Lee-ui-MacBookPro.local>
2021-05-04 21:56:33 +0900

    Add: users/views.py 작성

---
+++ yunhyunglee/utils.py
@@ -0,0 +1,15 @@
+import re
+
+def VALIDATOR_EMAIL(email):
+    validator = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+
+    if validator.match(email):
+        return True
+    return False
+
+def VALIDATOR_PASSWORD(password):
+    validator = re.compile('^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$')
+
+    if validator.match(password):
+        return True
+    return False

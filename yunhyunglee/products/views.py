ed2e9b2d805daca88ceb69543e15d06c78e5324b
Yunhyung Lee <yunhyunglee@Yunhyung-Lee-ui-MacBookPro.local>
2021-05-06 19:59:39 +0900

    Modify: 상품 리스트 페이지 수정

--- yunhyunglee/products/views.py
+++ yunhyunglee/products/views.py
@@ -29,6 +29,46 @@ class DestinationView(View):
         return JsonResponse({'data' : data}, status=200)
 
 
+class ProductView(View):
+    def get(self, request):
+        is_room = request.GET.get('is_room', None)
+        is_dinning = request.GET.get('is_dinning', None)
+
+        product_list = []
+
+        if is_room:
+            products = Product.objects.filter(is_room=True)
+
+            for product in products:
+                product_list.append({
+                    'name'     : product.name,
+                    'rating'   : product.rating,
+                    'city'     : product.city.name,
+                    'district' : product.district.name,
+                    'price'    : product.price,
+                    'image'    : product.productimage_set.first().image_url,
+                    'room_type'   : product.room.room_type.name,
+                    'star_rating' : product.room.star_rating
+                    })
+
+        if is_dinning:
+            products = Product.objects.filter(is_dinning=True)
+
+            for product in products:
+                product_list.append({
+                    'name'     : product.name,
+                    'rating'   : product.rating,
+                    'city'     : product.city.name,
+                    'district' : product.district.name,
+                    'price'    : product.price,
+                    'image'    : product.productimage_set.first().image_url,
+                    'dinning_type' : product.dinning.dinning_type.name,
+                    'food_type'    : product.dinning.food_type.name
+                    })
+
+        return JsonResponse({'data' : product_list}, status=200)
+
+
 class ProductDetailView(View):
     def get(self, request, product_id=None):
 
@@ -40,43 +80,39 @@ class ProductDetailView(View):
         product_list = []
 
         product_list.append({
-                'name' : product.name,
-                'rating' : product.rating,
+                'name'        : product.name,
+                'rating'      : product.rating,
                 'description' : product.description,
-                'address' : product.address,
-                'latitude' : product.latitude,
-                'longitude' : product.longitude,
-                'city'      : product.city.name,
-                'district' : product.district.name,
-                'price' : product.price,
-                'is_room' : product.is_room,
-                'is_dinning' : product.is_dinning,
+                'address'     : product.address,
+                'latitude'    : product.latitude,
+                'longitude'   : product.longitude,
+                'city'        : product.city.name,
+                'district'    : product.district.name,
+                'price'       : product.price,
+                'is_room'     : product.is_room,
+                'is_dinning'  : product.is_dinning,
                 'image' : [{
                     'image_url' : image.image_url
                     } for image in product.productimage_set.all()],
                 })
 
         if Product.objects.filter(id=product_id, is_room=True):
-            room = {
+            product_list.append({
                     'room_type'   : product.room.room_type.name,
                     'star_rating' : product.room.star_rating,
                     'convenience' : [{
                         'name'         : convenience.name,
                         'service_type' : convenience.service_type.name
                         } for convenience in product.room.convenience.all()]
-                    }
-
-            product_list.append(room)
+                    })
 
             return JsonResponse({'message' : product_list}, status=200)
 
         if Product.objects.filter(id=product_id, is_dinning=True):
-            dinning = {
+            product_list.append({
                     'description'  : product.dinning.description,
                     'food_type'    : product.dinning.food_type.name,
                     'dinning_type' : product.dinning.dinning_type.name
-                    }
-
-            product_list.append(dinning)
+                    })
 
             return JsonResponse({'data' : product_list}, status=200)

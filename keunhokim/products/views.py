576b6112295278f2ac9e5992b381cdb6b5b1665f
keunho_kim <astrocat9311@gmail.com>
2021-05-06 19:43:46 +0900

    nothing

--- keunhokim/products/views.py
+++ keunhokim/products/views.py
@@ -2,26 +2,47 @@ from django.views import View
 from .models      import *
 from django.http  import HttpResponse,JsonResponse
 
-#def category(request):
-#    if request.method == "GET":
-#        data = {
-#            'category':
-#        }
+def category(request):
+    if request.method == "GET":
+        categories = Category.objects.all()
 
-
-
-
-
-def DestinationMain(request):
-    if request.method == 'GET':
-        districts = City.objects.get(name='부산').district_set.all()
-
-        data = {
-            'name'     : [district.name for district in districts],
-            'image_url': [district.image_url for district in districts]
-        }
+        data = [{
+            'name'     : category.name,
+            'image_url': category.image_url
+        } for category in Category.objects.all()]
 
         return JsonResponse({'data':data},status=200)
+
+def destination(request,category_id):
+    if request.method == "GET":
+        destinations = Category.objects.get(id=category_id).destination.all()
+
+        data = [{
+            'name': destination.name,
+            'image_url': destination.image_url
+            } for destination in destinations]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

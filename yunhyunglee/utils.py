5bfca6b4679ecf4f4eb2065e753bca3b51866a81
Yunhyung Lee <yunhyunglee@Yunhyung-Lee-ui-MacBookPro.local>
2021-05-07 11:30:45 +0900

    Add: 쿠폰 기능 구현

--- yunhyunglee/utils.py
+++ yunhyunglee/utils.py
@@ -1,4 +1,12 @@
 import re
+import jwt
+import json
+
+from django.http  import JsonResponse
+
+from my_settings  import SECRET_KEY
+from users.models import User
+
 
 def VALIDATOR_EMAIL(email):
     validator = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
@@ -13,3 +21,22 @@ def VALIDATOR_PASSWORD(password):
     if validator.match(password):
         return True
     return False
+
+
+def login_check(func):
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            access_token = request.headers.get('Authorization', None)
+            payload      = jwt.decode(access_token, SECRET_KEY, algorithm='HS256')
+            user         = User.objects.get(id=payload['user'])
+            request.user = user
+
+        except jwt.exceptions.DecodeError:
+            return JsonResponse({'message' : 'INVALID_TOKEN'}, status=400)
+
+        except User.DoesNotExist:
+            return JsonResponse({'message' : 'INVALID_USER'}, status=400)
+
+        return func(self, request, *args, **kwargs)
+
+    return wrapper
